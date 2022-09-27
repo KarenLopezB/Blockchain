409fbdfa2af08d6f65717f736d3721796b23710e
karenlopezb <karen_lopez1936@hotmail.com>
2022-09-27 16:28:49 -0700

    Clase 27 de sep

--- src/services/index.js
+++ src/services/index.js
@@ -1,15 +1,21 @@
-import express from "express";
+import express  from "express";
 import bodyParser from "body-parser";
 
 import Blockchain from "../blockchain/blockchain";
 
-const { HTTP_PORT = 3000} = process.env;
+const {HTTP_PORT = 3000} = process.env;
 
 const app = express();
 const blockchain = new Blockchain();
 
 app.use(bodyParser.json());
+app.get('/blocks',(req,res) => {
+    res.json(blockchain.blocks)
+});
+
+
 
 app.listen(HTTP_PORT, () =>{
-    console.log(`Service HTTP: ${HTTP_PORT} listening...`);
+    
+    console.log(`Service HTTP: ${HTTP_PORT} listening...`)
 });
