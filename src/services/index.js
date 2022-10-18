e83c45f24bc3da7d8c69f4c68cecc81642fd4b30
karenlopezb <karen_lopez1936@hotmail.com>
2022-10-18 09:11:28 -0700

    P2P

--- src/services/index.js
+++ src/services/index.js
@@ -2,14 +2,17 @@ import express  from "express";
 import bodyParser from "body-parser";
 
 import Blockchain from "../blockchain/blockchain";
-import { restart } from "nodemon";
+//import { restart } from "nodemon";
+import P2PService from "./p2p";
 
 const {HTTP_PORT = 3000} = process.env;
 
 const app = express();
 const blockchain = new Blockchain();
+const p2pservice = new P2PService();
 
 app.use(bodyParser.json());
+
 app.get('/blocks',(req,res) => {
     res.json(blockchain.blocks)
 });
@@ -18,13 +21,13 @@ app.post('/mine', (req, res) => {
     const { body: { data } } = req;
     const block = blockchain.addBlock(data);
 
-    restart.json({
+    res.json({
         blocks: blockchain.blocks.length,
         block,
     });
 });
 
 app.listen(HTTP_PORT, () =>{
-    
-    console.log(`Service HTTP: ${HTTP_PORT} listening...`)
+    console.log(`Service HTTP: ${HTTP_PORT} listening...`);
+    p2pservice.listen();
 });
